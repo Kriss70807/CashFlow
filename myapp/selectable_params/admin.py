ca59f36ac40d33dfa79ab9fe5286a4d99215aa91
AlexandrKlepinin <alexandrklepinin@gmail.com>
2025-08-09 22:12:46 +0500

    Register models in admin, create some notes in admin-panel, download drf, create launch.json for django-debug

--- myapp/selectable_params/admin.py
+++ myapp/selectable_params/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categories, Statuses, Subcategories, Types_operations
+
+admin.site.register(Categories)
+admin.site.register(Statuses)
+admin.site.register(Subcategories)
+admin.site.register(Types_operations)
