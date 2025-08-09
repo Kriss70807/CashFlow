293734f59ce973fe987962be8fd3b58a6d517aba
AlexandrKlepinin <alexandrklepinin@gmail.com>
2025-08-09 21:53:56 +0500

    Create models in apps cash_flow and selectable_params, make migrations, create superuser

--- myapp/cash_flow/models.py
+++ myapp/cash_flow/models.py
@@ -1,3 +1,54 @@
+from datetime import date
+
 from django.db import models
 
-# Create your models here.
+from selectable_params.models import (
+    Statuses,
+    Types_operations,
+    Categories,
+    Subcategories,
+)
+
+
+class Cash_flow(models.Model):
+    """
+    Класс, представляющий собой данные о движениях денежных средств (далее по тексту - движение) в таблице cash_flow.
+
+    Атрибуты:
+        date_creation (date): дата создания записи о движении, заполняется автоматически значением текущей даты в формате дд.мм.гггг.
+        status_id (int): внешний ключ ссылающийся на таблицу statuses.
+        type_operation_id (int): внешний ключ, ссылающийся на таблицу types_operations.
+        category_id (int): внешний ключ, ссылающийся на таблицу categories.
+        subcategory_id (int): внешний ключ, ссылающийся на таблицу subcategories.
+        amount (int): размер денежных средств (сумма) с которыми было выполнено движение.
+        comment (str): комментарий свободной формы к выполненному движению.
+    """
+
+    date_creation = models.DateField(
+        default=date.today, verbose_name="Дата выполнения"
+    )
+
+    status = models.ForeignKey(
+        to=Statuses, on_delete=models.PROTECT, verbose_name="Статус"
+    )
+
+    type_operation = models.ForeignKey(
+        to=Types_operations, on_delete=models.PROTECT, verbose_name="Тип"
+    )
+
+    category = models.ForeignKey(
+        to=Categories, on_delete=models.PROTECT, verbose_name="Категория",
+    )
+
+    subcategory = models.ForeignKey(
+        to=Subcategories, on_delete=models.PROTECT, verbose_name="Подкатегория"
+    )
+
+    amount = models.IntegerField(null=False, verbose_name="Сумма")
+    comment = models.TextField(max_length=300, null=True, verbose_name="Комментарий")
+
+    class Meta:
+        db_table = "cash_flow"
+        verbose_name = "Движение денежных средств"
+        verbose_name_plural = "Движения денежных средств"
+        ordering = ("date_creation",)
--- myapp/selectable_params/models.py
+++ myapp/selectable_params/models.py
@@ -1,3 +1,90 @@
+from typing import Literal
+
 from django.db import models
 
-# Create your models here.
+
+class Statuses(models.Model):
+    """
+    Класс, представляющий собой данные о статусах движений денежных средств в таблице statuses.
+
+    Атрибут:
+        name: хранит название статуса в качестве значения.
+    """
+
+    name = models.CharField(max_length=100, verbose_name="Статус")
+
+    class Meta:
+        db_table: str = "statuses"
+        verbose_name: str = "Статус"
+        verbose_name_plural: str = "Статусы"
+        ordering: tuple[Literal["name"]] = ("name",)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Types_operations(models.Model):
+    """
+    Класс, представляющий собой данные о типах движений денежных средств в таблице types_operations.
+
+    Атрибут:
+        name: хранит название типа в качестве значения.
+    """
+
+    name = models.CharField(max_length=100, verbose_name="Тип")
+
+    class Meta:
+        db_table: str = "types_operations"
+        verbose_name: str = "Тип"
+        verbose_name_plural: str = "Типы"
+        ordering: tuple[Literal["name"]] = ("name",)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Categories(models.Model):
+    """
+    Класс, представляющий собой данные о категориях движений денежных средств в таблице categories.
+
+    Атрибут:
+        name: хранит название категории в качестве значения.
+    """
+
+    name = models.CharField(max_length=100, verbose_name="Категория")
+    type_operation = models.ForeignKey(
+        to=Types_operations, on_delete=models.CASCADE, verbose_name="Тип", editable=True
+    )
+
+    class Meta:
+        db_table: str = "categories"
+        verbose_name: str = "Категория"
+        verbose_name_plural: str = "Категории"
+        ordering: tuple[Literal["name"]] = ("name",)
+    
+    def __str__(self) -> str:
+        return self.name
+
+
+class Subcategories(models.Model):
+    """
+    Класс, представляющий собой данные о подкатегориях движений денежных средств в таблице subcategories.
+
+    Атрибут:
+        name: хранит название подкатегории в качестве значения.
+        category: является внешним ключом, ссылающим на таблицу categories.
+    """
+
+    name = models.CharField(max_length=100, verbose_name="Подкатегория")
+    category = models.ForeignKey(
+        to=Categories, on_delete=models.CASCADE, verbose_name="Категория", editable=True
+    )
+
+    class Meta:
+        db_table: str = "subcategories"
+        verbose_name: str = "Подкатегория"
+        verbose_name_plural: str = "Подкатегории"
+        ordering: tuple[Literal["name"]] = ("name",)
+    
+    def __str__(self) -> str:
+        return self.name
